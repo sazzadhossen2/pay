fe3c6e94249bee009931fa4044c40b02e0121fd4
sazzad hossen <hossain15-5144@diu.edu.bd>
2026-03-01 21:42:31 +0600

    done

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 module.exports = {
   port: process.env.PORT || 5000,
-  baseUrl: process.env.BASE_URL || "http://localhost:5000",
+  baseUrl: process.env.BASE_URL || "https://payment-ub97.onrender.com",
   mongodbUri:
     process.env.MONGODB_URI || "mongodb://localhost:27017/subscription_payment",
   frontendUrl: process.env.FRONTEND_URL || "http://localhost:8080",
